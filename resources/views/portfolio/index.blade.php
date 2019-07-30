a7f2533a4da1ea7b2277edd6210cc5cb25fccfad
Yurii Salashnyi <yurik.salashnuy@gmail.com>
2019-07-30 15:50:06 +0300

    Some fixes

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -13,12 +13,12 @@
         <li class="tab-mnu__link">{{ $category->name }}</li>
       @endforeach
     </ul>
-  
+
     <div class="tab-content active">
       <div class="row">
       @foreach($list as $portfolio)
         @if(($loop->iteration % 3 + 1) == 0)</div><div class="row">@endif
-        
+
         <div class="col-sm-6 col-md-4">
           <a href="{{ route('portfolio.detail', $portfolio) }}" class="portfolio-item">
             <div class="portfolio-item__img">
@@ -31,27 +31,29 @@
       @endforeach
       </div>
     </div>
-    
+
     @foreach($categories as $category)
       <div class="tab-content">
         <div class="row">
           @forelse($category->portfolios as $portfolio)
             @if(($loop->iteration % 3 + 1) == 0)</div><div class="row">@endif
-        
+
           <div class="col-md-4">
             <a href="{{ route('portfolio.detail', $portfolio) }}" class="portfolio-item">
-              <div class="portfolio-item__img" style="background-image:url({{ asset('storage/' . $portfolio->cover) }})"></div>
+              <div class="portfolio-item__img">
+                <img src="{{ asset('storage/' . $portfolio->cover) }}" alt="">
+              </div>
               <h4 class="portfolio-item__ttl">{{ $portfolio->name }}</h4>
               <p class="portfolio-item__date">{{ $portfolio->completion_date->format('Y') }}</p>
             </a>
           </div>
-          
+
           @empty
             <div class="col-md-12 pb-4">@lang('dhi.portfolio.empty-category')</div>
           @endforelse
         </div>
       </div>
     @endforeach
-    
+
   </div>
 @endsection
