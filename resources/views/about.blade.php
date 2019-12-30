b2c8433081c81a21c445b3a9da5c2d9a2105b6b4
Yurii Salashnyi <yurik.salashnuy@gmail.com>
2019-12-30 22:13:09 +0200

    Frontend improvements

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -20,48 +20,78 @@
         </div>
         <div class="about__content">
           @if(app()->getLocale() == 'ru')
-            <p class="about__txt"><strong>Design Hub International</strong> – это команда экспертов в создании эффективных архитектурных решений. Мы архитекторы, инженеры, урбанисты и дизайнеры с большим опытом работы в проектировании жилой и коммерческой недвижимости.</p>
-            <p class="about__txt">Наше бюро предоставляет полный спектр услуг в области архитектурного проектирования, инженерии, строительства, консалтинга, генерального планирования и дизайна интерьеров. Начиная от идеи проекта и до полной его реализации.</p>
-            <p class="about__txt">Компания, с главным офисом в Киеве и представительством в Роттердаме, более 6 лет, успешно работает над проектами не только на территории СНГ, но и за его пределами, предоставляя высококачественные услуги для наших международных клиентов.</p>
-            <p class="about__txt"><strong>Мы предлагаем</strong> коммерчески эффективные архитектурные решения, которые удовлетворяют как потребности бизнеса, так и желания потребителя. Профессионализм и предпринимательский подход к работе позволяют нам создавать проекты, которые помогают оптимизировать расходы наших клиентов, получить максимальный результат и удовлетворить потребности конечного потребителя.</p>
-            <p class="about__txt"><strong>Наша миссия</strong> – создавать архитектурные проекты, которые будут актуальны и через десятилетия. Используя многогранный подход к планированию и дизайну – placemaking approach – мы стремимся создавать комфортную городскую среду, благодаря которой интересы бизнеса и конечного потребителя не конкурируют между собой, а дополняют и усиливают друг друга.</p>
+            <p class="about__txt"><strong>Design Hub International</strong> - это команда экспертов в создании
+              эффективных
+              архитектурных решений. Мы архитекторы, инженеры, урбанисты и дизайнеры с большим опытом работы в
+              проектировании жилой и коммерческой недвижимости.</p>
+            <p class="about__txt">Наше бюро предоставляет полный спектр услуг в области архитектурного проектирования,
+              инженерии, строительства, консалтинга, генерального планирования и дизайна интерьеров. Начиная от идеи
+              проекта и до полной его реализации.</p>
+            <p class="about__txt">Компания, с главным офисом в Киеве и представительством в Роттердаме, более 6 лет,
+              успешно работает над проектами не только на территории СНГ, но и за его пределами, предоставляя
+              высококачественные услуги для наших международных клиентов.</p>
+            <p class="about__txt"><strong>Мы предлагаем</strong> коммерчески эффективные архитектурные решения, которые
+              удовлетворяют как потребности бизнеса, так и желания потребителя. Профессионализм и предпринимательский
+              подход к работе позволяют нам создавать проекты, которые помогают оптимизировать расходы наших клиентов,
+              получить максимальный результат и удовлетворить потребности конечного потребителя.</p>
+            <p class="about__txt"><strong>Наша миссия</strong> - создавать архитектурные проекты, которые будут
+              актуальны и
+              через десятилетия. Используя многогранный подход к планированию и дизайну – placemaking approach – мы
+              стремимся создавать комфортную городскую среду, благодаря которой интересы бизнеса и конечного потребителя
+              не конкурируют между собой, а дополняют и усиливают друг друга.</p>
             <h3 class="mb-3">Основные преимущества работы с Design Hub International:</h3>
-            <p class="about__txt"><strong>Efficient solutions.</strong> Создаем коммерчески эффективные решения для наших клиентов. Мы готовы разделить риски, стать соинвесторами вашего проекта и получить до 50% оплаты в м² либо долю в бизнесе.</p>
-            <p class="about__txt"><strong>Smart management.</strong> Мы верим в оптимизацию ресурсов и гибкие интегрированные команды. Мы сокращаем наши затраты, за счет привлечения необходимой команды, в соответствии с масштабом проекта, сложностью и реальными потребностями.</p>
-            <p class="about__txt"><strong>International experience.</strong> Благодаря большому опыту проектирования за границей – Голландия, Великобритания, Австралия – и широкой партнёрской сети, у нас есть возможность привлекать ведущие международные архитектурные, инжиниринговые и консалтинговые компании под свои задачи и проекты.</p>
-            <p class="about__txt"><strong>Project Cost Reduction.</strong> При создании проекта совместно с западными архитекторами, мы значительно сокращаем стоимость концепции для заказчика, за счет выполнения части работ по адаптации концепции к локальному рынку. Благодаря такому подходу наши клиенты экономят время, деньги и что немаловажно могут использовать имя зарубежной архитектурной студии в своих маркетинговых целях.</p>
+            <p class="about__txt"><strong>Efficient solutions.</strong> Создаем коммерчески эффективные решения для
+              наших клиентов. Мы готовы разделить риски, стать соинвесторами вашего проекта и получить до 50% оплаты в
+              м² либо долю в бизнесе.</p>
+            <p class="about__txt"><strong>Smart management.</strong> Мы верим в оптимизацию ресурсов и гибкие
+              интегрированные команды. Мы сокращаем наши затраты, за счет привлечения необходимой команды, в
+              соответствии с масштабом проекта, сложностью и реальными потребностями.</p>
+            <p class="about__txt"><strong>International experience.</strong> Благодаря большому опыту проектирования за
+              границей – Голландия, Великобритания, Австралия – и широкой партнёрской сети, у нас есть возможность
+              привлекать ведущие международные архитектурные, инжиниринговые и консалтинговые компании под свои задачи и
+              проекты.</p>
+            <p class="about__txt"><strong>Project Cost Reduction.</strong> При создании проекта совместно с западными
+              архитекторами, мы значительно сокращаем стоимость концепции для заказчика, за счет выполнения части работ
+              по адаптации концепции к локальному рынку. Благодаря такому подходу наши клиенты экономят время, деньги и
+              что немаловажно могут использовать имя зарубежной архитектурной студии в своих маркетинговых целях.</p>
           @else
-          <p class="about__txt">Design Hub International is an integrated design practice with its HQ office based in Kiev
-            and representative office in Rotterdam, which provides architectural, engineering, building consultancy,
-            design & planning services in CIS region and beyond, offering a broad range of professional architectural
-            services for our international partners.</p>
-          <p class="about__txt">The DHI Architecture’s professional ethos extends equally to the documentation and
-            delivery of our design as to the design itself. Our professional team includes highly experienced specialists
-            in all aspects of project delivery. Highly developed documentation quality assurance systems are a fundamental
-            part of our culture. Our offices are equipped with state-of-the-art computer design, visualization and
-            documentation systems, with access to common databases and libraries. These systems enable us to provide a
-            coordinated output of consistently high quality.</p>
-          <p class="about__txt">Design Development, Construction Documentation, and Construction Phase services for
-            building projects are highly specialized processes. Throughout the delivery phase, we exercise our proven
-            experience, from the exact detailing of the project’s myriad components and the rigors of coordinating the
-            developed architectural design with the other project consultant disciplines, to the scrupulous review of shop
-            drawings. We understand the built work quality standards, which must be achieved for the user experience, and
-            we document our work with the highest level of attention to detail, durability of the building’s performance
-            and economy of upkeep. Building industry research and development is a major part of our culture, with our
-            vast library and professional staff attending construction industry seminars and other ongoing research
-            programs.</p>
-          <p class="about__txt">Our industry over recent years has witnessed an exponential rate in the emergence of new
-            technologies in both design and construction. As building design professionals, we strive to stay abreast of
-            these new technologies, methods and systems, in order to best serve our Clients and our profession.</p>
-          <p class="about__txt">Our Kiev office has developed and maintains a Quality Management System achieves designs
-            which demonstrate an acceptable level of quality, aesthetic and commercial viability in the finished design
-            which substantially meets the Client’s requirements. To achieve this, key aspects of our system include:</p>
-          <p class="about__txt">• precise definition of our commission with our Client, and regular communication with and
-            response to the Client, and the Client group, concerning project requirements.<br>• efficient management of
-            the people and teams involved in the project.<br>• staff understanding of their responsibilities and
-            authorities in respect of the project.<br>• the testing of the design in a formal manner so that it meets the
-            Client’s requirements in accordance with the contractual arrangements.<br>• promotion of an effective risk
-            management culture.</p>
+            <p class="about__txt"><strong>Design Hub International</strong> is an integrated design practice with its HQ
+              office based in Kiev and representative office in Rotterdam, which provides architectural, engineering,
+              building consultancy,
+              design & planning services in CIS region and beyond, offering a broad range of professional architectural
+              services for our international partners.</p>
+            <p class="about__txt">Our bureau provides a full range of services in the field of architectural design,
+              engineering, construction, consulting, masterplanning and interior design. Starting from the idea of​the
+              project to its full implementation.</p>
+            <p class="about__txt"><strong>We offer</strong> commercially effective architectural solutions that satisfy
+              both the needs of
+              the business and the desires of the consumer. Professionalism and an entrepreneurial approach to work
+              allow us to create projects that help optimize the costs of our clients, get the maximum result, and
+              satisfy the needs of the end consumer.</p>
+            <p class="about__txt"><strong>Our mission</strong> is to create architectural projects that will be relevant
+              even after
+              decades. Using a multifaceted approach to planning and design - a placemaking approach - we strive to
+              create a comfortable urban environment, thanks to which the interests of business and the end consumer do
+              not compete with each other, but complement and strengthen one another.</p>
+            <h3 class="mb-3"><strong>Key benefits of working with Design Hub International:</strong></h3>
+            <p class="about__txt"><strong>Efficient solutions.</strong> Creating commercially efficient solutions for
+              our clients. We are
+              ready to share the risks, become co-investors and get up to 50% of the payment in sqm or a share in the
+              business.</p>
+            <p class="about__txt"><strong>Smart management.</strong> We believe in resource optimization and flexible
+              integrated teams.
+              We reduce our costs by involving only the core specialists to the project team, depending on the scale,
+              complexity and real project's needs.</p>
+            <p class="about__txt"><strong>International experience.</strong> Due to our global design experience –
+              Netherlands, UK,
+              Australia – and an extensive partnership network, we have the opportunity to involve leading international
+              architectural, engineering and consulting companies into our projects on a collaboration basis.</p>
+            <p class="about__txt"><strong>Project Cost Reduction.</strong> When creating a project with foreign
+              architects, we can
+              significantly reduce the cost of the design project for the client due to the implementation of part of
+              the work on adapting the concept to the local market. Thanks to this approach, our customers save time,
+              money, and, what is not less important, can use the name of a famous architectural studio for their
+              marketing purposes.</p>
           @endif
         </div>
       </div>
--- resources/views/technologies.blade.php
+++ resources/views/technologies.blade.php
@@ -16,9 +16,15 @@
           <div class="technologies__content">
             <h4 class="technologies__subttl">Информационное моделирование зданий</h4>
             <p class="technologies__txt">
-              Мы используем самые передовые технологии BIM, моделирования, рендеринга и постпродакшена, которые может предложить рынок. Одновременно каждый член команды может работать в одном файле Team Work.<br><br>
-              Рабочий процесс, совместное использование и хранение файлов, организованы с использованием облачных технологий, чтобы обеспечить бесперебойную командную работу как внутри команды разработчиков, так и с представителями клиента.<br><br>
-              Наша главная цель - добиться эффективной и интегрированной синхронизации архитектуры, интерьеров и инженерных услуг с помощью BIM моделирования. Это делает наше внутреннее и внешнее сотрудничество и дальнейшую работу над документацией, моделированием, CGI или производством анимации единым цельным процессом.
+              Мы используем самые передовые технологии BIM, моделирования, рендеринга и постпродакшена, которые может
+              предложить рынок. Одновременно каждый член команды может работать в одном файле Team Work.<br><br>
+              Рабочий процесс, совместное использование и хранение файлов, организованы с использованием облачных
+              технологий, чтобы обеспечить бесперебойную командную работу как внутри команды разработчиков, так и с
+              представителями клиента.<br><br>
+              Наша главная цель - добиться эффективной и интегрированной синхронизации архитектуры, интерьеров и
+              инженерных услуг с помощью BIM моделирования. Это делает наше внутреннее и внешнее сотрудничество и
+              дальнейшую работу над документацией, моделированием, CGI или производством анимации единым цельным
+              процессом.
             </p>
           </div>
         </div>
@@ -28,7 +34,11 @@
           <div class="technologies__content">
             <h4 class="technologies__subttl">Параметрический дизайн</h4>
             <p class="technologies__txt">
-              Проектирование или создание дизайна с использованием математических алгоритмов и вычислений позволяет нам создавать сложные шаблоны, целые массивы или различные поверхности. Алгоритм создан так, чтобы компьютер после ввода всех данных рассчитал наиболее эргономичную модель. Если вы измените один параметр, программа немедленно изменит все конфигурации. Это значительно ускоряет рабочий процесс и снижает затраты на проектирование.
+              Проектирование или создание дизайна с использованием математических алгоритмов и вычислений позволяет нам
+              создавать сложные шаблоны, целые массивы или различные поверхности. Алгоритм создан так, чтобы компьютер
+              после ввода всех данных рассчитал наиболее эргономичную модель. Если вы измените один параметр, программа
+              немедленно изменит все конфигурации. Это значительно ускоряет рабочий процесс и снижает затраты на
+              проектирование.
             </p>
           </div>
         </div>
@@ -37,40 +47,40 @@
         </div>
       </div>
     @else
-    <div class="row technologies__wrap">
-      <div class="col-lg-6 col-md-6">
-        <img src="img/tech_img1.jpg" alt="DHI" class="technologies__img img-r">
-      </div>
-      <div class="col-lg-6 col-md-6">
-        <div class="technologies__content">
-          <h4 class="technologies__subttl">Building Information Modeling</h4>
-          <p class="technologies__txt">
-            We exploit the most advanced BIM, modeling, rendering and post-production technologies which the market can
-            offer. At the same time, each and every team member can work in a single Team Work file.<br><br>
-            The work process and file sharing and storage is organized using cloud technologies to enable seamless teamwork
-            within design team as well as with client’s representatives.<br><br>
-            Our overall aim is to achieve an effective and integrated synchronisation of architecture, interiors and
-            engineering services through BIM design process which makes our internal and external collaboration and further
-            work on documentation, modelling and CGI’s / animation production as a single seamless process.
-          </p>
+      <div class="row technologies__wrap">
+        <div class="col-lg-6 col-md-6">
+          <img src="img/tech_img1.jpg" alt="DHI" class="technologies__img img-r">
+        </div>
+        <div class="col-lg-6 col-md-6">
+          <div class="technologies__content">
+            <h4 class="technologies__subttl">Building Information Modeling</h4>
+            <p class="technologies__txt">
+              We exploit the most advanced BIM, modeling, rendering and post-production technologies that the market can
+              offer. At the same time, each team member can work in a single Team Work file.<br><br>
+              The work process, file sharing and storage are organized using cloud technologies to enable seamless
+              teamwork within the design team as well as with client’s representatives.<br><br>
+              Our overall aim is to achieve an effective and integrated synchronization of architecture, interiors and
+              engineering services through BIM design process. This makes our internal and external collaboration and
+              further work on documentation, modeling, CGIs, or animation production as a single seamless process.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="row technologies__wrap">
-      <div class="col-lg-6 order-2 order-md-1 col-md-6">
-        <div class="technologies__content">
-          <h4 class="technologies__subttl">Parametric design</h4>
-          <p class="technologies__txt">
-            Designing or creating a design using mathematical algorithms and calculations. Allows you to create complex
-            patterns, whole arrays or different surfaces. Created to ensure that the computer, upon inputting all the data,
-            has calculated the most ergonomic model. If you change one parameter, the program immediately changes all
-            configurations. It greatly accelerates the workflow and reduces design costs.</p>
+      <div class="row technologies__wrap">
+        <div class="col-lg-6 order-2 order-md-1 col-md-6">
+          <div class="technologies__content">
+            <h4 class="technologies__subttl">Parametric design</h4>
+            <p class="technologies__txt">
+              Designing or creating a design using mathematical algorithms and calculations allows us to create complex
+              patterns, whole arrays, or different surfaces. Created to ensure that the computer, upon inputting all the
+              data, has calculated the most ergonomic model. If you change one parameter, the program immediately
+              changes all configurations. It greatly accelerates the workflow and reduces design costs.</p>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 order-1 order-md-2">
+          <img src="img/tech_img2.jpg" alt="DHI" class="technologies__img img-r">
         </div>
       </div>
-      <div class="col-lg-6 col-md-6 order-1 order-md-2">
-        <img src="img/tech_img2.jpg" alt="DHI" class="technologies__img img-r">
-      </div>
-    </div>
     @endif
   </div>
 @endsection
