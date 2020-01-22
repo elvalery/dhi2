7819d3d209c6410982e03693dc9f0061d9366684
Elena Voloshyna <elvalery@gnolltech.com>
2020-01-22 18:52:13 +0200

    Add justify plugin to ckeditor

--- app/Admin/Extensions/Form/CKEditor.php
+++ app/Admin/Extensions/Form/CKEditor.php
@@ -16,7 +16,7 @@ class CKEditor extends Field
   public function render()
   {
     //$this->script = "$('textarea.{$this->getElementClassString()}').ckeditor();";
-    $this->script = "CKEDITOR.replace('{$this->column}', options);";
+    $this->script = "CKEDITOR.replace('{$this->column}', {extraPlugins: 'justify'});";
 
 
     return parent::render();
