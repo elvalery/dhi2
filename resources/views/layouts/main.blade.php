6ba660640c6ece967fd85631c35d5451d352aa1a
Elena Voloshyna <elvalery@relicorn.com>
2020-06-05 22:28:16 +0300

    Fix title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title>@section('title')Design Hub International@show</title>
+  <title>@section('title')Design Hub International @show</title>
   <meta name="description" content="@yield('description')">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
