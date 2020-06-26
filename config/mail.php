2d723599528e8a324a727e44cdbdf5f58b6980ad
Elena Voloshyna <elvalery@relicorn.com>
2020-06-26 14:03:39 +0300

    Add one more email to receive contacts requests

--- config/mail.php
+++ config/mail.php
@@ -60,7 +60,7 @@ return [
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
-  'to' => [env('MAIL_TO'), 'info@dhi-architecture.com', 'mariya@woohoo.marketing'],
+  'to' => [env('MAIL_TO'), 'info@dhi-architecture.com', 'mariya@woohoo.marketing', 'y.andrey@gmail.com'],
 
     /*
     |--------------------------------------------------------------------------
